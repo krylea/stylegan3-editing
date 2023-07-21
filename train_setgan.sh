bc29e6ee6245936ebbaeac2d8f22194766ed38f4
Kira Selby <kaselby@uwaterloo.ca>
2023-07-21 14:58:57 -0400

    updates

--- train_setgan.sh
+++ train_setgan.sh
@@ -12,10 +12,8 @@ BATCH_PER_GPU=16
 
 RES=$1
 DATASET_NAME=$2
-ENC_PATH=$3
-DEC_PATH=$4
-ckpt=${5:-''}
-kimg=${6:-10000}
+ckpt=${3:-''}
+kimg=${4:-10000}
 
 BATCH=$((BATCH_PER_GPU * SLURM_GPUS_ON_NODE)) 
 GPUS=$SLURM_GPUS_ON_NODE
@@ -39,8 +37,6 @@ fi
 argstring="$argstring \
 --encoder_type ProgressiveBackboneEncoder \
 --input_nc 3 \
---stylegan_weights $DEC_PATH \
---checkpoint_path $ENC_PATH \
 --n_styles 18 \
 --reference_size 7 12 \
 --candidate_size 1 4"
