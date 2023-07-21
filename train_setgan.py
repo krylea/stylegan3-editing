9546278ee71e88d3335ecf6d6733ea1dee74d014
Kira Selby <kaselby@uwaterloo.ca>
2023-07-21 15:22:38 -0400

    updates

--- train_setgan.py
+++ train_setgan.py
@@ -237,7 +237,7 @@ def main(**kwargs):
     c.total_kimg = opts.kimg
     c.kimg_per_tick = opts.tick
     c.image_snapshot_ticks = c.network_snapshot_ticks = opts.snap
-    c.random_seed = c.training_set_kwargs.random_seed = opts.seed
+    c.random_seed = opts.seed
     c.data_loader_kwargs.num_workers = opts.workers
 
     # Sanity checks.
