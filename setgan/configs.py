ba6951cd9b585190b157882240364c04e01ccc1c
Kira Selby <kaselby@uwaterloo.ca>
2023-07-25 11:00:03 -0400

    updates

---
+++ setgan/configs.py
@@ -0,0 +1,21 @@
+
+
+dataset_paths = {
+	'face': '../setgan2/datasets/vggface/train/',
+    'imagenet': '/scratch/hdd001/datasets/imagenet/train'
+}
+
+
+model_paths = {
+	# models for backbones and losses
+	'ir_se50': 'pretrained_models/model_ir_se50.pth',
+	'curricular_face': 'pretrained_models/CurricularFace_Backbone.pth',
+	'mtcnn_pnet': 'pretrained_models/mtcnn/pnet.npy',
+	'mtcnn_rnet': 'pretrained_models/mtcnn/rnet.npy',
+	'mtcnn_onet': 'pretrained_models/mtcnn/onet.npy',
+
+    # encoders and decoders
+    'styleganxl_face_256_encoder': 'experiments/test-ffhq-1/checkpoint.pt',
+    'styleganxl_face_256': '../stylegan-xl/pretrained_models/ffhq256.pkl',
+
+}
