25e0de8adb53563a631474d4c618397aa43157c8
Kira Selby <kaselby@uwaterloo.ca>
2023-07-25 10:21:27 -0400

    updates

--- setgan/dataset.py
+++ setgan/dataset.py
@@ -170,25 +170,9 @@ class ImagesDataset(Dataset):
 
         return im
     
-    def _get_zipfile(self):
-        assert self._type == 'zip'
-        if self._zipfile is None:
-            self._zipfile = zipfile.ZipFile(self._path)
-        return self._zipfile
-
-    def _open_file(self, fname):
-        if self._type == 'dir':
-            return open(os.path.join(self._path, fname), 'rb')
-        if self._type == 'zip':
-            return self._get_zipfile().open(fname, 'r')
-        return None
-    
     def _load_raw_image(self, fname, idx):
-        with self._open_file(fname) as f:
-            if pyspng is not None and self._file_ext(fname) == '.png':
-                image = pyspng.load(f.read())
-            else:
-                image = np.array(PIL.Image.open(f))
+        with open(fname, 'rb')as f:
+            image = np.array(PIL.Image.open(f))
 
         image = self._preprocess(image, idx)
 
