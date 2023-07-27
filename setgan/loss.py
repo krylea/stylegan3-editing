2a18e35978848a868d237c996eb3b2d7c37d627e
kiras <kaselby@uwaterloo.ca>
2023-07-27 14:54:37 -0400

    debug

--- setgan/loss.py
+++ setgan/loss.py
@@ -91,8 +91,8 @@ class ProjectedSetGANLoss(Loss):
             reference_set = to_imgset(ref_flat, initial_set=reference_set)
             imgs = to_imgset(imgs_flat, initial_set=imgs)
 
-        #return self.D(reference_set, imgs)
-        return self.D(to_images(imgs), None)
+        return self.D(reference_set, imgs)
+       
 
     def accumulate_gradients(self, phase, reference_set, candidate_set, gen_s, gain, cur_nimg):
         assert phase in ['Gmain', 'Greg', 'Gboth', 'Dmain', 'Dreg', 'Dboth']
