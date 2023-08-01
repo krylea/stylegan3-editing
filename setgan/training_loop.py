2375436f506b19a75d868e26bd137ce6a754408a
kiras <kaselby@uwaterloo.ca>
2023-08-01 13:45:45 -0400

    refactoring train and add iters arg for restyle

--- setgan/training_loop.py
+++ setgan/training_loop.py
@@ -446,9 +446,9 @@ def training_loop(
                 torch.distributed.barrier()
 
         # Save image snapshot.
-        if (rank == 0) and (image_snapshot_ticks is not None) and (done or cur_tick % image_snapshot_ticks == 0):
-            images = torch.cat([G_ema(z=z, c=c, noise_mode='const').cpu() for z, c in zip(grid_z, grid_c)]).numpy()
-            save_image_grid(images, os.path.join(run_dir, f'fakes{cur_nimg//STEP_INTERVAL:06d}.png'), drange=[-1,1], grid_size=grid_size)
+        #if (rank == 0) and (image_snapshot_ticks is not None) and (done or cur_tick % image_snapshot_ticks == 0):
+        #    images = torch.cat([G_ema(z=z, c=c, noise_mode='const').cpu() for z, c in zip(grid_z, grid_c)]).numpy()
+        #    save_image_grid(images, os.path.join(run_dir, f'fakes{cur_nimg//STEP_INTERVAL:06d}.png'), drange=[-1,1], grid_size=grid_size)
 
         # Save network snapshot.
         snapshot_pkl = None
